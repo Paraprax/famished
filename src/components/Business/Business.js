62e0c17d76e58f56c56d05ef3da7bb84d15aafb6
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-05 20:58:58 -0400

    created pizza object in Business.js && imported React

---
+++ src/components/Business/Business.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const business = {
+  imageSrc: "https://i.ytimg.com/vi/qh9yn9vGNzg/maxresdefault.jpg",
+  name: "Mr. Aziz' Pizza",
+  address: "1962 Waterfront Way",
+  city: "Manhattan",
+  state: "NY",
+  zipCode: "2004",
+  category: "Take-Out",
+  rating: 4.2,
+  reviewCount: 202,
+};
