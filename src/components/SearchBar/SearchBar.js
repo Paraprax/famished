a3f00d57d29e216b550afa321e32e805b477087b
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-07 18:46:36 -0400

    object of string params for querying the Yelp API

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SearchBar.css";
+
+const optionsForSortBy = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviews": "review_count",
+};
