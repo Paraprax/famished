b368dd7a5d090d0aedde109c9cdcfcd8c71ef148
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-07 19:00:55 -0400

    SearchBar component subclass with renderSortOptions method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./SearchBar.css";
 
+//query strings for sort_by endpoint of the Yelp API:
 const optionsForSortBy = {
   "Best Match": "best_match",
   "Highest Rated": "rating",
   "Most Reviews": "review_count",
 };
+
+//search bar component:
+
+class SearchBar extends React.Component {
+  renderSortOptions() {
+    Object.keys(optionsForSortBy).map((option) => {
+      let optionValue = optionsForSortBy[option];
+      return <li key={optionValue}>{option}</li>;
+    });
+  }
+}
