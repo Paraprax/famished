7d430f708547b99ee69c8210925be9d6a07c12ea
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-12 23:09:55 -0400

    moved demo business object(aziz pizza) into App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,18 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+  imageSrc: "https://i.ytimg.com/vi/qh9yn9vGNzg/maxresdefault.jpg",
+  name: "Mr. Aziz' Pizza",
+  address: "1962 Waterfront Way",
+  city: "Manhattan",
+  state: "NY",
+  zipCode: "2004",
+  category: "Take-Out",
+  rating: 4.2,
+  reviewCount: 202,
+};
+
 class App extends React.Component {
   render() {
     return (
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,18 +1,6 @@
 import React from "react";
 import "./Business.css";
 
-const business = {
-  imageSrc: "https://i.ytimg.com/vi/qh9yn9vGNzg/maxresdefault.jpg",
-  name: "Mr. Aziz' Pizza",
-  address: "1962 Waterfront Way",
-  city: "Manhattan",
-  state: "NY",
-  zipCode: "2004",
-  category: "Take-Out",
-  rating: 4.2,
-  reviewCount: 202,
-};
-
 class Business extends React.Component {
   render() {
     return (
