d6c90262832e1a92d685d63ac9c9082714d7596f
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-19 18:11:57 -0400

    testing CORS-anywhere API to bypass CORS restriction issue

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,8 @@ const yelpApiKey = yelpKey;
 
 const Yelp = {
   search(term, location, sortBy) {
-    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    const CORSanywhere = "https://cors-anywhere.herokuapp.com/";
+    const yelpUrl = `${CORSanywhere}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
     return fetch(yelpUrl);
   },
 };
