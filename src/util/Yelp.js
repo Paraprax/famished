c6d7e525a05c1901f87ea345ebdd2bc4ebf33267
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-19 18:15:00 -0400

    headers object for fetch request

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -5,6 +5,11 @@ const Yelp = {
   search(term, location, sortBy) {
     const CORSanywhere = "https://cors-anywhere.herokuapp.com/";
     const yelpUrl = `${CORSanywhere}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    const headersObject = {
+      headers: {
+        Authorization: `Bearer: ${yelpKey}`,
+      },
+    };
     return fetch(yelpUrl);
   },
 };
