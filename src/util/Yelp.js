9a444bf1322193fb1e98fd42e1f8071148ca0bbb
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-19 16:31:22 -0400

    initial Yelp object setup w/ search method && url construction

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,9 @@
 import { yelpKey } from "./yelpkey";
 const yelpApiKey = yelpKey;
+
+const Yelp = {
+  search(term, location, sortBy) {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    return fetch(yelpUrl);
+  },
+};
