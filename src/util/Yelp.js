5576981e7a26541a53cfcb00055f43a9cbf17880
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-19 19:15:09 -0400

    formatting object of properties accessed from jsonResponse

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -16,7 +16,24 @@ const Yelp = {
       })
       .then((jsonResponse) => {
         if (jsonResponse.businesses) {
-        }
-      });
-  },
-};
+          console.log(jsonResponse);
+          console.log("Yelp module triggered");
+          const restaurants = jsonResponse.businesses.map((restaurant) => {
+            return {
+              id: restaurant.id,
+              imageSrc: restaurant.image_url,
+              name: restaurant.name,
+              address: restaurant.location.address1,
+              city: restaurant.location.city,
+              state: restaurant.location.state,
+              zipCode: restaurant.location.zip_code,
+              category: restaurant.categories[0].title,
+              rating: restaurant.rating,
+              reviewCount: restaurant.review_count,
+            }; //return object
+          }); //restaurants callback
+          return restaurants;
+        } //if-statement
+      }); //then callback
+  }, //search object
+}; //yelp object
