06d36a3bb8f106c16c1bae72ec462f4b431429d6
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-19 23:29:59 -0400

    round restaurant images; borders

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,7 +6,6 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {/* {console.log(this.props.businesses)} */}
         {this.props.businesses.map((business) => {
           return <Business key={business.id} business={business} />;
         })}
