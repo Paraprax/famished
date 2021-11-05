ce2582585d67d4e9f00789ee6e90786dc61ea373
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-11-04 20:41:57 -0400

    Add city suffix to address query

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,7 +7,9 @@ class BusinessList extends React.Component {
     return (
       <div className="BusinessList">
         {this.props.businesses.map((business) => {
-          return <Business key={business.id} business={business} googleURL={`https://www.google.com/maps/search/?api=1&query=${business.name}+${business.city}`} addressURL={`https://www.google.com/maps/search/?api=1&query=${business.address}`} />;
+          return (
+            <Business key={business.id} business={business} googleURL={`https://www.google.com/maps/search/?api=1&query=${business.name}+${business.city}`} addressURL={`https://www.google.com/maps/search/?api=1&query=${business.address}+${business.city}`} />
+          );
         })}
       </div>
     );
